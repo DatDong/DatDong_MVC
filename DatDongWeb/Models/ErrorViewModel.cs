81239636e63acee3d024f0539f90f102e2c5d8a2
DatDong <phatdat16102002@gmail.com>
2024-01-20 11:42:14 +0700

    Add project files.

---
+++ DatDongWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatDongWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
